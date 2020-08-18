84b4f1f8321843a79203239e3d713fc968c0b81a
MwebesaEmmanuel <49569630+MwebesaEmmanuel@users.noreply.github.com>
2020-08-18 14:51:17 +0300

    Add files via upload

---
+++ frontend2.js
@@ -0,0 +1,10 @@
+
+function Checking(){
+    let txtpassword= document.getElementById("txtpassword").Value;
+    
+    alert(username+ "thank you u can continue please");
+}
+function welcome(){
+    
+     alert("Hello welcome here")
+}
